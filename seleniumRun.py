d2c69a36e81a8bbf63ee7644863d07260bfd2179
Maleke <malekelthomas97@gmail.com>
2020-05-06 17:59:41 -0400

    Put credentials in a separate file and added ability to login

--- seleniumRun.py
+++ seleniumRun.py
@@ -7,17 +7,29 @@ from selenium import webdriver
 
 browser = webdriver.Firefox()
 
+logininfo = []
+
+with open("credentials.txt") as f:
+	for line in f.readlines():
+		logininfo.append(line)
+	f.close()
+
 
 browser.get('https://www.instagram.com/')
 
-usernameInput = browser.find_element_by_name("username") #finds HTML element with username tag, in this case the username text field
+usernameInput = browser.find_element_by_name("username") #finds HTML element by name field, in this case the username text field
 
-usernameInput.send_keys("lekedraws") #enters text into field 
+usernameInput.send_keys(logininfo[0]) #enters text into field 
+
 
 passwordInput = browser.find_element_by_name("password")
 
-passwordInput.send_keys("pw")
+passwordInput.send_keys(logininfo[1])
 
-browser.sleep(5)
+login_button = browser.find_element_by_xpath("//button[@type='submit']")
+
+login_button.click()
+
+sleep(5)
 
 browser.close()
